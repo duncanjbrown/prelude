d719348e51c7dbe2d915c63dd7c04922cb6e3569
Duncan Brown <duncanjbrown@gmail.com>
2019-09-19 20:31:22 +0100

    Add enh-ruby-mode
    - disable the magic 'utf-8' comment
    - don't go crazy when indenting parens

--- personal/ruby.el
+++ personal/ruby.el
@@ -1,1 +1,3 @@
-(prelude-require-package 'rspec-mode)
+(prelude-require-packages '(enh-ruby-mode rspec-mode ruby-electric))
+(setq enh-ruby-deep-indent-paren nil)
+(setq ruby-insert-encoding-magic-comment nil)
