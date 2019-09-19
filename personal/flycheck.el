03088c17e4fc3ce6f02adc76dcb0c0d105b05511
Duncan Brown <duncanjbrown@gmail.com>
2019-09-19 20:29:34 +0100

    Disable flycheck in org mode
    Otherwise it interferes with <C-c !>

--- personal/flycheck.el
+++ personal/flycheck.el
@@ -1,4 +1,6 @@
-(prelude-require-package 'flycheck-pos-tip)
+(prelude-require-packages '(flycheck-pos-tip flycheck))
+
+(setq flycheck-global-modes '(not org-mode))
 (setq global-flycheck-mode 1)
 
 (with-eval-after-load 'flycheck
